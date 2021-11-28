0d70b4157e07056c29a75c97886d1c226e025713
karol <karol@top-team.pl>
2021-11-28 13:30:58 +0100

    .gra

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 from pygame.locals import (
     RLEACCEL,
-    KEYUP,
+    K_UP,
     K_DOWN,
     K_LEFT,
     K_RIGHT,
@@ -18,4 +18,111 @@ SCREEN_HEIGHT = 900
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.image.load("resources\\jest.png").convert()
+        self.surf = pygame.image.load("resources\\jet.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+            moveUpSound.play()
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+            moveDownSound.play()
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load("resources\\missile.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center = (
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT)
+            )
+        )
+        self.speed = random.randint(5, 10)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+
+
+pygame.mixer.init()
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+
+player = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+enemies = pygame.sprite.Group()
+
+pygame.mixer.music.load("resources\\01_Opening_Credits.mp3")
+pygame.mixer.music.play(loops=-1)
+
+moveUpSound = pygame.mixer.Sound("resources\\Rising_putter.ogg")
+moveDownSound = pygame.mixer.Sound("resources\\Falling_putter.ogg")
+collisionSound = pygame.mixer.Sound("resources\\Collision.ogg")
+
+moveUpSound.set_volume(0.5)
+moveDownSound.set_volume(0.5)
+collisionSound.set_volume(0.5)
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    enemies.update()
+
+    screen.fill((0, 0, 0))
+
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    if pygame.sprite.spritecollideany(player, enemies):
+        player.kill()
+
+        moveUpSound.stop()
+        moveDownSound.stop()
+        collisionSound.stop()
+
+        running = False
+
+    pygame.display.flip()
+    clock.tick(30)
+
+pygame.mixer.music.stop()
+pygame.mixer.quit()
